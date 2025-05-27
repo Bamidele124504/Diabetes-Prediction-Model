5015b5c6ec269060ac8f21b4003e831ad10cc21e
Bamidele-obazu <Obazubamidele@gmail.com>
2025-05-27 11:51:00 +0100

    Update Diabetes prediction web app.py

--- Diabetes prediction web app.py
+++ Diabetes prediction web app.py
@@ -9,7 +9,7 @@ import pickle
 import streamlit as st
 
 #loading the saved model
-loaded_model=pickle.load(open('C:/Users/lenovo/Desktop/python learning/trained_model.sav','rb'))
+loaded_model=pickle.load(open('trained_model.sav','rb'))
 
 #creating A function for prediction
 def diabetes_prediction(input_data):
